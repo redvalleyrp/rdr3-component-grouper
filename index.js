c32ae571288e9b62cf1e18c2091867f51e7ac95e
Jaime Filho <jaimeadanielfilho@hotmail.com>
2022-03-03 14:54:46 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+
+class Component {
+    constructor(category, variation) {
+        this.category = category;
+        this.variation = variation;
+    }
+
+    toString() {
+        if (this.variation) {
+            return `${this.category}_${this.variation}`;
+        }
+
+        return this.category;
+    }
+
+    static parse(text) {
+        const match = text.match(/(?<category>(.+\d+)|(HORSE_EQUIPMENT_HORN_NEW))_(?<variation>.+)/);
+
+        if (match) {
+            return new Component(match.groups.category, match.groups.variation);
+        }
+        
+        return new Component(text);
+    }
+}
+
+class Group {
+    constructor(category) {
+        this.category = category;
+        this.items = []
+    }
+}
+
+function groupComponentsByCategory(componentKeys) {
+    const groupedComponentKeys = []
+
+    let currentGroup = undefined;
+
+    for (const componentKey of componentKeys) {
+        if (typeof componentKey === 'string') {
+            const component = Component.parse(componentKey);
+
+            if (currentGroup === undefined) {
+                currentGroup = new Group(component.category);
+            }
+
+            if (currentGroup.category !== component.category) {
+                groupedComponentKeys.push(currentGroup.items);
+                currentGroup = new Group(component.category);
+            }
+
+            currentGroup.items.push(componentKey);
+        } else {
+            groupedComponentKeys.push(componentKey)
+        }
+    }
+
+    return groupedComponentKeys;
+}
+
+function groupComponentLists(lists) {
+    if (Array.isArray(lists)) {
+        return groupComponentByCategory(lists);
+    }
+
+    const groupedLists = {}
+
+    for (const [key, value] of Object.entries(lists)) {
+        groupedLists[key] = groupComponentList(value);
+    }
+
+    return groupedLists;
+}
+
+fs.writeFileSync(
+    './grouped_components.json',
+    JSON.stringify(groupComponentList(require('./components.json')), undefined, 4)
+);
